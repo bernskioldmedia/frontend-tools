f2ac3ad4cf5922cd684d814eb915de8e2f697ae3
Erik Bernskiold <erik@bernskioldmedia.com>
2020-10-04 12:03:04 +0200

    Remove lodash usage

--- packages/map/index.js
+++ packages/map/index.js
@@ -1,5 +1,4 @@
 import GoogleMapsApi from "./googleMapsApi";
-import _ from "lodash";
 import markerTemplate from "./markerTemplate";
 
 /**
@@ -13,9 +12,9 @@ export default class Map {
 	 * @param selector
 	 * @param options
 	 */
-	constructor( selector, options = {} ) {
+	constructor(selector, options = {}) {
 
-		this.options  = options;
+		this.options = options;
 		this.selector = selector;
 
 		this.setupMaps();
@@ -29,17 +28,17 @@ export default class Map {
 		const apiKey = document.body.dataset.googleApiKey ? document.body.dataset.googleApiKey : undefined;
 
 		// If we don't have an API key, stop loading and show error.
-		if ( apiKey === undefined ) {
-			console.error( "A Google API key must be set in the settings to be able to load the Google Maps." );
+		if (apiKey === undefined) {
+			console.error("A Google API key must be set in the settings to be able to load the Google Maps.");
 			return;
 		}
 
-		const api  = new GoogleMapsApi( apiKey );
-		const maps = document.querySelectorAll( this.selector );
+		const api = new GoogleMapsApi(apiKey);
+		const maps = document.querySelectorAll(this.selector);
 
-		api.load().then( () => {
-			this.loadMaps( maps );
-		} );
+		api.load().then(() => {
+			this.loadMaps(maps);
+		});
 
 	}
 
@@ -48,16 +47,16 @@ export default class Map {
 	 *
 	 * @param maps
 	 */
-	loadMaps( maps ) {
+	loadMaps(maps) {
 
-		if ( ! window.google ) {
-			console.error( "Could not properly load the Google API. Map has not been initialized." );
+		if (!window.google) {
+			console.error("Could not properly load the Google API. Map has not been initialized.");
 			return;
 		}
 
-		maps.forEach( ( map ) => {
-			this.renderMap( map );
-		} );
+		maps.forEach((map) => {
+			this.renderMap(map);
+		});
 
 	}
 
@@ -66,32 +65,35 @@ export default class Map {
 	 *
 	 * @param mapEl
 	 */
-	renderMap( mapEl ) {
-		const markers = mapEl.dataset.markers ? JSON.parse( mapEl.dataset.markers ) : {};
+	renderMap(mapEl) {
+		const markers = mapEl.dataset.markers ? JSON.parse(mapEl.dataset.markers) : {};
 
-		const showControls = ( "true" === mapEl.dataset.showControls ) ? true : false;
+		const showControls = ("true" === mapEl.dataset.showControls) ? true : false;
 
 		const mapOptions = {
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			zoom: parseFloat( mapEl.dataset.zoom ? mapEl.dataset.zoom : 12 ),
+			zoom: parseFloat(mapEl.dataset.zoom ? mapEl.dataset.zoom : 12),
 			center: {
-				lat: parseFloat( mapEl.dataset.lat ? mapEl.dataset.lat : 0 ),
-				lng: parseFloat( mapEl.dataset.lng ? mapEl.dataset.lng : 0 ),
+				lat: parseFloat(mapEl.dataset.lat ? mapEl.dataset.lat : 0),
+				lng: parseFloat(mapEl.dataset.lng ? mapEl.dataset.lng : 0),
 			},
-			disableDefaultUI: ! showControls,
+			disableDefaultUI: !showControls,
 		};
 
-		const options = { ...mapOptions, ...this.options.mapOptions };
+		const options = {
+			...mapOptions,
+			...this.options.mapOptions
+		};
 
-		const map = new google.maps.Map( mapEl, options );
+		const map = new google.maps.Map(mapEl, options);
 
 		/**
 		 * Render markers if they exist.
 		 */
-		if ( _.isEmpty( markers ) ) {
-			console.info( "No markers were found for the map." );
+		if (markers.length === 0) {
+			console.info("No markers were found for the map.");
 		} else {
-			this.renderMarkers( map, markers );
+			this.renderMarkers(map, markers);
 		}
 
 	}
@@ -102,30 +104,30 @@ export default class Map {
 	 * @param map
 	 * @param markers
 	 */
-	renderMarkers( map, markers ) {
+	renderMarkers(map, markers) {
 
 		const icon = {
-			scaledSize: new google.maps.Size( 80, 80 ),
+			scaledSize: new google.maps.Size(80, 80),
 		};
 
-		markers.forEach( ( markerData ) => {
+		markers.forEach((markerData) => {
 
-			const template = this.options.markerTemplate ? this.options.markerTemplate( markerData ) : markerTemplate( markerData );
+			const template = this.options.markerTemplate ? this.options.markerTemplate(markerData) : markerTemplate(markerData);
 
-			const marker = new google.maps.Marker( {
-				position: new google.maps.LatLng( markerData.lat, markerData.lng ),
+			const marker = new google.maps.Marker({
+				position: new google.maps.LatLng(markerData.lat, markerData.lng),
 				map: map,
 				// icon: icon,
 				title: markerData.title,
 				content: template,
 				animation: google.maps.Animation.DROP,
-			} );
+			});
 
 			const infoWindow = new google.maps.InfoWindow();
 
-			this.onMarkerClick( map, marker, infoWindow );
+			this.onMarkerClick(map, marker, infoWindow);
 
-		} );
+		});
 
 	}
 
@@ -136,18 +138,18 @@ export default class Map {
 	 * @param marker
 	 * @param infoWindow
 	 */
-	onMarkerClick( map, marker, infoWindow ) {
+	onMarkerClick(map, marker, infoWindow) {
 
-		google.maps.event.addListener( marker, "click", function() {
-			infoWindow.setContent( marker.content );
-			infoWindow.open( map, marker );
-		} );
+		google.maps.event.addListener(marker, "click", function () {
+			infoWindow.setContent(marker.content);
+			infoWindow.open(map, marker);
+		});
 
-		google.maps.event.addListener( map, "click", function() {
-			if ( infoWindow ) {
-				infoWindow.close( map, infoWindow );
+		google.maps.event.addListener(map, "click", function () {
+			if (infoWindow) {
+				infoWindow.close(map, infoWindow);
 			}
-		} );
+		});
 
 	}
 
